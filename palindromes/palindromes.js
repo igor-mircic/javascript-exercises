63b4ec3bcb5a4d22ee02893b124172e2a3420528
Igor Mircic <igor.mircic@live.com>
2021-02-21 21:41:57 +0100

    finished palindromes.js

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function() {
+const palindromes = function (string = "") {
+  // * Conver to lower case, remove sapces, and punctuation * //
+  string = string.toLowerCase(); // Convert to lower case
+  string = string.split(" ").join(""); // Remove spaces
+  const punctRE = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g; // regx for punctuation
+  string = string.replace(punctRE, ""); // remove punctuation
 
-}
+  // * Reverse string * //
+  let reversedString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+  // * Check if string is palindrom * //
+  return string === reversedString ? true : false;
+};
 
-module.exports = palindromes
+module.exports = palindromes;
