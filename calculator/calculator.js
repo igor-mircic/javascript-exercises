3eb963b4f79fcaf8d6f3826da7be18a730fe37fc
Igor Mircic <igor.mircic@live.com>
2021-02-21 20:56:04 +0100

    finished calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,32 +1,46 @@
-function add () {
-	
+function add(x, y) {
+  return x + y;
 }
 
-function subtract () {
-	
+function subtract(x, y) {
+  return x - y;
 }
 
-function sum () {
-	
+function sum(arrayToSum = []) {
+  let sum = 0;
+  arrayToSum.forEach((number) => {
+    sum += number;
+  });
+  return sum;
 }
 
-function multiply () {
-	
+function multiply(arrayToMultiply = []) {
+  let multiply = 1;
+  arrayToMultiply.forEach((number) => {
+    multiply *= number;
+  });
+  return multiply;
 }
 
-function power() {
-	
+function power(x, y) {
+  return x ** y;
 }
+// 2 <= 1
+function factorial(number) {
+  if (number === 0) return 1;
 
-function factorial() {
-	
+  let product = 1;
+  for (let i = 2; i <= number; i++) {
+    product *= i;
+  }
+  return product;
 }
 
 module.exports = {
-	add,
-	subtract,
-	sum,
-	multiply,
-    power,
-	factorial
-}
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial,
+};
