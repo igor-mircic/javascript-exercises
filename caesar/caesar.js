fdf8edec7119b8f3f2d81abfa1fee9b379854a34
Igor Mircic <igor.mircic@live.com>
2021-02-22 16:46:34 +0100

    refactored caesar.js

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,33 +1,26 @@
-const caesar = function (string = "", shiftBy = 1) {
-  let encodedString = "";
-  for (let i = 0; i < string.length; i++) {
-    const char = string.charCodeAt(i);
-    if (shiftBy > 26) {
-      shiftBy = shiftBy - Math.floor(shiftBy / 26) * 26;
-    }
-    if (char >= 65 && char <= 90) {
-      if (char + shiftBy > 90) {
-        encodedString += String.fromCharCode(64 + (char + shiftBy - 90));
-      } else if (char + shiftBy < 65) {
-        encodedString += String.fromCharCode(90 - (64 - (char + shiftBy)));
-      } else {
-        encodedString += String.fromCharCode(char + shiftBy);
-      }
-    } else if (char >= 97 && char <= 122) {
-      if (char + shiftBy > 122) {
-        encodedString += String.fromCharCode(96 + (char + shiftBy - 122));
-      } else if (char + shiftBy < 97) {
-        encodedString += String.fromCharCode(122 - (96 - (char + shiftBy)));
-      } else {
-        encodedString += String.fromCharCode(char + shiftBy);
-      }
-    } else {
-      encodedString += String.fromCharCode(char);
-    }
-  }
-  return encodedString;
+const caesar = function (string, shift) {
+  return string
+    .split("")
+    .map((char) => shiftChar(char, shift))
+    .join("");
 };
 
-console.log(caesar("D", -5));
+const codeSet = (code) => (code < 97 ? 65 : 97);
+
+// this function is just a fancy way of doing % so that it works with negative numbers
+// see this link for details:
+// https://stackoverflow.com/questions/4467539/javascript-modulo-gives-a-negative-result-for-negative-numbers
+const mod = (n, m) => ((n % m) + m) % m;
+
+const shiftChar = (char, shift) => {
+  const code = char.charCodeAt();
+
+  if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+    return String.fromCharCode(
+      mod(code + shift - codeSet(code), 26) + codeSet(code)
+    );
+  }
+  return char;
+};
 
 module.exports = caesar;
