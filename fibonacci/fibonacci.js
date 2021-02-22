87726ac7386049dc9cfc9aea3e8d0745ff1ba973
Igor Mircic <igor.mircic@live.com>
2021-02-22 20:03:02 +0100

    finished fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (typeof n === "string") n = parseInt(n);
+  if (n < 0) return "OOPS";
+  if (n === 0) return 0;
 
-}
+  //   let f = [1, 1];
 
-module.exports = fibonacci
+  let a = 1;
+  let b = 1;
+
+  for (let i = 2; i < n; i++) {
+    const tmp = b;
+    b = a + b;
+    a = tmp;
+    // f[i + 1] = f[i] + f[i - 1];
+  }
+  return b;
+};
+
+module.exports = fibonacci;
