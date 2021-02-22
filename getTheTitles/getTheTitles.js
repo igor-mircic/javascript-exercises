f4d104121fdbd491849792c8de9a7ec6b021718b
Igor Mircic <igor.mircic@live.com>
2021-02-22 20:29:51 +0100

    finished getTheTitles.js

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
-}
+const getTheTitles = function (books) {
+  allTitles = [];
+  for (let i = 0; i < books.length; i++) {
+    const book = books[i];
+    allTitles[i] = book.title;
+    console.log(book.title);
+  }
+  return allTitles;
+};
 
 module.exports = getTheTitles;
